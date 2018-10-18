6a0515bbde1b846cff0ee4ee3b9a70d36e8863f8
Jul Jen <juliajen.prog@gmail.com>
2018-10-18 03:52:10 +0000

    automatically backed up by learn

--- app/workers/leads_worker.rb
+++ app/workers/leads_worker.rb
@@ -4,6 +4,7 @@ class LeadsWorker
   # that takes in whatever data is required to complete the job
   include Sidekiq::Worker
 
+# loop that processes the file in our controller, and just put it inside of perform. Make sure to require 'csv' at the top, and update the CSV.foreach to work with leads_file.
   def perform(leads_file)
     CSV.foreach(leads_file, headers: true) do |lead|
       Customer.create(email: lead[0], first_name: lead[1], last_name: lead[2])
